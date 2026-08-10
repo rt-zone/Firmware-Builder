58760c8be114fa179b94e90d8904e792606d53c2
atbrbk-fedora <atb.rbk@gmail.com>
2026-08-10 18:02:57 +0500

    pngle fixed on non-ubuntu os

--- boards/PiBody/cmodules/st7789/png/pngle.h
+++ boards/PiBody/cmodules/st7789/png/pngle.h
@@ -44,7 +44,8 @@ typedef void (*pngle_done_callback_t)(pngle_t *pngle);
 // ----------------
 // Basic interfaces
 // ----------------
-pngle_t *pngle_new();
+struct _st7789_ST7789_obj_t;
+pngle_t *pngle_new(struct _st7789_ST7789_obj_t *self);
 void pngle_destroy(pngle_t *pngle);
 void pngle_reset(pngle_t *pngle); // clear its internal state (not applied to pngle_set_* functions)
 const char *pngle_error(pngle_t *pngle);
